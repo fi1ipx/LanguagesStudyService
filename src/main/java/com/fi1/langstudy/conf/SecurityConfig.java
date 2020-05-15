0cf5964c434cde01039fc5cf432413903717f183
Philipp Masharov <fmasharov@gridgain.com>
2020-05-15 09:19:57 +0300

    Spring Security modifications

--- src/main/java/com/fi1/langstudy/conf/SecurityConfig.java
+++ src/main/java/com/fi1/langstudy/conf/SecurityConfig.java
@@ -1,16 +1,25 @@
 package com.fi1.langstudy.conf;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
     private static final String MATCH_ALL = "/**";
+
+    protected DataSource dataSource;
 
     @Value("${langstudy.security.enabled:true}")
     private boolean securityEnabled;
@@ -23,15 +32,42 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .antMatchers(MATCH_ALL)
                     .authenticated()
                     .and()
-                    .httpBasic();
+                    .formLogin();
         } else {
             http
                     .authorizeRequests()
                     .antMatchers(MATCH_ALL)
-                    .permitAll()
-                    .and()
-                    .httpBasic();
+                    .permitAll();
+            http.csrf().disable();
         }
-        http.csrf().disable();
+    }
+
+    @Bean
+    public JdbcUserDetailsManager userDetailsManager() {
+        final JdbcUserDetailsManager manager = new JdbcUserDetailsManager();
+        manager.setDataSource(dataSource);
+        manager.setUsersByUsernameQuery(
+                "select username,password,enabled from study.users where username=?");
+        manager.setAuthoritiesByUsernameQuery(
+                "select username, role from study.user_roles where username=?");
+        manager.setRolePrefix("ROLE_");
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder builder)
+            throws Exception {
+
+        builder.userDetailsService(userDetailsManager());
+    }
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 }
